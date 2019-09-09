b03507e1a808686c584bd90a76b2c8f64da87eb8
rhkrm <52055050+rhkrm@users.noreply.github.com>
2019-09-09 13:14:40 +0900

    Create 2750.py

---
+++ acmicpc/2750.py
@@ -0,0 +1,12 @@
+def solution() :
+    N = int(input())
+
+    num_list = []
+    for _ in range(N) :
+        num_list.append(int(input()))
+
+    num_list.sort()
+    for i in num_list :
+        print(i)
+
+solution()
