e1c40a4b8e005e524c4c2ca184daa1ce7be0daf2
rhkrm <52055050+rhkrm@users.noreply.github.com>
2019-09-09 13:11:59 +0900

    Update 2798.py

--- acmicpc/2798.py
+++ acmicpc/2798.py
@@ -1,25 +1,14 @@
 def solution() :
-    # N, M = map(int, input().split())
-    # card = list(map(int, input().split()))
-    # 
-    # # card.sort(reverse=1)
-    # Max = 0
-    # for i in range(N-2) :
-    #     for j in range(i+1,N-1) :
-    #         for k in range (j+1, N) :
-    #             if card[i]+card[j]+card[k] <= M :
-    #                 Max = max(Max, card[i]+card[j]+card[k])
-    # 
-    # print(Max)
     N, M = map(int, input().split())
-    card = list(map(int, input().split(' ')))
-    answer = 0
-    for i in range(0, N - 2):
-        for j in range(i + 1, N - 1):
-            for k in range(j + 1, N):
-                if (card[i] + card[j] + card[k]) <= M and M - (card[i] + card[j] + card[k]) < M - answer:
-                    answer = card[i] + card[j] + card[k]
-    print(answer)
+    card = list(map(int, input().split()))
+
+    Max = 0
+    for i in range(N-2) :
+        for j in range(i+1,N-1) :
+            for k in range (j+1, N) :
+                if card[i]+card[j]+card[k] <= M :
+                    Max = max(Max, card[i]+card[j]+card[k])
+    print(Max)
 
 
 solution()
